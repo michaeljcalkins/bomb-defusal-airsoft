9be0331843b3d0ef06eca48785f5ba327c24e1c6
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-10-25 13:48:58 -0700

    Change name and settings

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,212 +5,233 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-	public float secondsUntilExplosion;
+    public float secondsUntilExplosion;
 
-	private AudioSource alarmSound;
-	private AudioSource defusedSound;
-	private Text percentText;
-	private Text timerText;
-	private Renderer progressBar;
-	private bool isArmed = false;
-	private bool isArming = false;
-	private bool isDisarming = false;
-	private float timeRemaining;
-	private double startTimestamp;
-	private double endTimestamp;
-	private double explodeTimestamp;
-	private bool isSpaceDown = false;
-	private float progress = 0;
-	private double progressBarBlinkTimer;
-	private bool onoff;
-	private Vector2 progressBarStartingPosition = new Vector2 (0, 25000);
-	private bool isPlaying = false;
+    private AudioSource alarmSound;
+    private AudioSource defusedSound;
+    private Text percentText;
+    private Text timerText;
+    private Renderer progressBar;
+    private bool isArmed = false;
+    private bool isArming = false;
+    private bool isDisarming = false;
+    private float timeRemaining;
+    private double startTimestamp;
+    private double endTimestamp;
+    private double explodeTimestamp;
+    private bool isSpaceDown = false;
+    private float progress = 0;
+    private double progressBarBlinkTimer;
+    private bool onoff;
+    private bool isPlaying = false;
 
-	// Use this for initialization
-	void Start ()
-	{
-		percentText = GameObject.Find ("PercentText").GetComponent<Text> ();
-		timerText = GameObject.Find ("TimerText").GetComponent<Text> ();
-		alarmSound = GameObject.Find ("AlarmAudioClip").GetComponent<AudioSource> ();
-		defusedSound = GameObject.Find ("DefusedAudioClip").GetComponent<AudioSource> ();
-		progressBar = GameObject.Find ("ProgressBar").GetComponent<Renderer> ();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        percentText = GameObject.Find("PercentText").GetComponent<Text>();
+        timerText = GameObject.Find("TimerText").GetComponent<Text>();
+        alarmSound = GameObject.Find("AlarmAudioClip").GetComponent<AudioSource>();
+        defusedSound = GameObject.Find("DefusedAudioClip").GetComponent<AudioSource>();
+        progressBar = GameObject.Find("ProgressBar").GetComponent<Renderer>();
+    }
 
-	// Update is called once per frame
-	void Update ()
-	{
-		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
-			isSpaceDown = true;
-		} else if (Input.GetKeyUp (KeyCode.Space) || Input.GetMouseButtonUp (0)) {
-			isSpaceDown = false;
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            isSpaceDown = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            isSpaceDown = false;
+        }
 
-		if (
-			isArmed &&
-			GetTime () > (explodeTimestamp + 10000)) {
-			isArming = false;
-			isDisarming = false;
-			isArmed = false;
-			progress = 0;
-			explodeTimestamp = 0;
-		}
+        if (
+            isArmed &&
+            GetTime() > (explodeTimestamp + 10000))
+        {
+            isArming = false;
+            isDisarming = false;
+            isArmed = false;
+            progress = 0;
+            explodeTimestamp = 0;
+        }
 
-		// Render bomb progress
-		if (
-			(isArming || isDisarming) &&
-			isSpaceDown &&
-			(progress > 0 || progress < 5000)) {
-			progress = isArming ? progress + Time.deltaTime * 1000 : progress - Time.deltaTime * 1000;
-		} else {
-			progress = isArmed ? 5000 : 0;
-		}
+        // Render bomb progress
+        if (
+            (isArming || isDisarming) &&
+            isSpaceDown &&
+            (progress > 0 || progress < 5000))
+        {
+            progress = isArming ? progress + Time.deltaTime * 1000 : progress - Time.deltaTime * 1000;
+        }
+        else
+        {
+            progress = isArmed ? 5000 : 0;
+        }
 
-		UpdateProgressBarSize ();
+        UpdateProgressBarSize();
 
-		// Updates the display text
-		percentText.text = GetPercentString ();
-		timerText.text = GetRemainingTimeText ();
+        // Updates the display text
+        percentText.text = GetPercentString();
+        timerText.text = GetRemainingTimeText();
 
-		percentText.enabled = !isArmed;
-		timerText.enabled = isArmed;
+        percentText.enabled = !isArmed;
+        timerText.enabled = isArmed;
 
-		ProgressBarBlink ();
+        ProgressBarBlink();
 
-		if (isArmed) {
-			ProgressBarBlink ();
-		}
+        if (isArmed)
+        {
+            ProgressBarBlink();
+        }
 
-		// Nothing should be in progress when not holding down space
-		if (!isSpaceDown) {
-			isArming = false;
-			isDisarming = false;
-		}
+        // Nothing should be in progress when not holding down space
+        if (!isSpaceDown)
+        {
+            isArming = false;
+            isDisarming = false;
+        }
 
-		// Stop playing the audio if nothing is happening
-		if (!isSpaceDown && !isArmed && !isArming) {
-			alarmSound.Stop ();
-			isPlaying = false;
-		}
+        // Stop playing the audio if nothing is happening
+        if (!isSpaceDown && !isArmed && !isArming)
+        {
+            alarmSound.Stop();
+            isPlaying = false;
+        }
 
-		// Player pushed space to arm the bomb
-		if (!isArmed && isSpaceDown && progress < 5000) {
-			if (!isPlaying) {
-				alarmSound.Play ();
-				isPlaying = true;
-			}
+        // Player pushed space to arm the bomb
+        if (!isArmed && isSpaceDown && progress < 5000)
+        {
+            if (!isPlaying)
+            {
+                alarmSound.Play();
+                isPlaying = true;
+            }
 
-			isArming = true;
-			// 40 seconds * 1000 milliseconds = 40,000 ms
-			explodeTimestamp = GetTime () + (secondsUntilExplosion * 1000);
+            isArming = true;
+            // 40 seconds * 1000 milliseconds = 40,000 ms
+            explodeTimestamp = GetTime() + (secondsUntilExplosion * 1000);
 
-			return;
-		}
+            return;
+        }
 
-		// Player attempting to disarm the bomb
-		if (isArmed && !isArming && isSpaceDown && progress > 0) {
-			isDisarming = true;
+        // Player attempting to disarm the bomb
+        if (isArmed && !isArming && isSpaceDown && progress > 0)
+        {
+            isDisarming = true;
 
-			return;
-		}
+            return;
+        }
 
-		// Bomb armed
-		if (progress >= 5000 && isArming) {
-			ArmBomb ();
+        // Bomb armed
+        if (progress >= 5000 && isArming)
+        {
+            ArmBomb();
 
-			return;
-		}
+            return;
+        }
 
-		// Bomb disarmed
-		if (progress <= 0 && isDisarming) {
-			DisarmBomb ();
+        // Bomb disarmed
+        if (progress <= 0 && isDisarming)
+        {
+            DisarmBomb();
 
-			return;
-		}
-	}
+            return;
+        }
+    }
 
-	void ProgressBarBlink ()
-	{
-		if (isArming || isDisarming) {
-			progressBar.material.color = Color.red;
-			return;
-		}
+    void ProgressBarBlink()
+    {
+        if (isArming || isDisarming)
+        {
+            progressBar.material.color = Color.red;
+            return;
+        }
 
-		if (Time.time > progressBarBlinkTimer) {
-			progressBarBlinkTimer = Time.time + .3;
-			onoff = !onoff;
+        if (Time.time > progressBarBlinkTimer)
+        {
+            progressBarBlinkTimer = Time.time + .3;
+            onoff = !onoff;
 
-			if (onoff) {
-				progressBar.material.color = Color.red;
-			} else {
-				progressBar.material.color = Color.yellow;
-			}
-		}
-	}
+            if (onoff)
+            {
+                progressBar.material.color = Color.red;
+            }
+            else
+            {
+                progressBar.material.color = Color.yellow;
+            }
+        }
+    }
 
-	private string GetPercentString ()
-	{
-		return (progress / 5000 * 100).ToString ("0") + "%";
-	}
+    private string GetPercentString()
+    {
+        return (progress / 5000 * 100).ToString("0") + "%";
+    }
 
-	void UpdateProgressBarSize ()
-	{
-		float width = Camera.main.orthographicSize * 2 * Screen.width / Screen.height;
-		float height = Camera.main.orthographicSize * 2 * 2;
+    void UpdateProgressBarSize()
+    {
+        float width = Camera.main.orthographicSize * 2 * Screen.width / Screen.height;
+        float height = Camera.main.orthographicSize * 2 * 2;
 
-		// Set the size of the progress bar
-		progressBar.transform.localScale = new Vector3 (progress / 5000 * width * 2, height, 0);
+        // Set the size of the progress bar
+        progressBar.transform.localScale = new Vector3(progress / 5000 * width * 2, height, 0);
 
-		// Posistion the progress bar in the bottom left of the screen
-		progressBar.transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (0, 0, 190));
-	}
+        // Posistion the progress bar in the bottom left of the screen
+        progressBar.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 190));
+    }
 
-	private double GetTime ()
-	{
-		System.DateTime epochStart = new System.DateTime (1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
-		int currentTimestamp = (int)(System.DateTime.UtcNow - epochStart).TotalSeconds;
+    private double GetTime()
+    {
+        System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+        int currentTimestamp = (int)(System.DateTime.UtcNow - epochStart).TotalSeconds;
 
-		return currentTimestamp * 1000;
-	}
+        return currentTimestamp * 1000;
+    }
 
-	private string GetRemainingTimeText ()
-	{
-		int secondsRemaining = Mathf.Min ((int)((explodeTimestamp - GetTime ()) / 1000), 49);
-			
-		if (secondsRemaining <= 0) {
-			return "0:00";
-		}
+    private string GetRemainingTimeText()
+    {
+        int secondsRemaining = Mathf.Min((int)((explodeTimestamp - GetTime()) / 1000), 49);
 
-		int minutes = secondsRemaining / 60;
-		int seconds = secondsRemaining - minutes * 60;
+        if (secondsRemaining <= 0)
+        {
+            return "0:00";
+        }
 
-		if (seconds < 10) {
-			return "0:0" + seconds.ToString ();
-		}
+        int minutes = secondsRemaining / 60;
+        int seconds = secondsRemaining - minutes * 60;
 
-		return "0:" + seconds.ToString ();
-	}
+        if (seconds < 10)
+        {
+            return "0:0" + seconds.ToString();
+        }
 
-	void ArmBomb ()
-	{
-		print ("Bomb armed");
+        return "0:" + seconds.ToString();
+    }
 
-		progress = 5000;
-		isArmed = true;
-		isArming = false;
-	}
+    void ArmBomb()
+    {
+        print("Bomb armed");
 
-	void DisarmBomb ()
-	{
-		print ("Bomb disarmed");
+        progress = 5000;
+        isArmed = true;
+        isArming = false;
+    }
 
-		alarmSound.Stop ();
-		defusedSound.Play ();
-		isPlaying = false;
+    void DisarmBomb()
+    {
+        print("Bomb disarmed");
 
-		progress = 0;
-		isArmed = false;
-		isArming = false;
-		isDisarming = false;
-		explodeTimestamp = 0;
-	}
+        alarmSound.Stop();
+        defusedSound.Play();
+        isPlaying = false;
+
+        progress = 0;
+        isArmed = false;
+        isArming = false;
+        isDisarming = false;
+        explodeTimestamp = 0;
+    }
 }
